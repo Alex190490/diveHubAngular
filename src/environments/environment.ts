6e55a15675499e4abcec7c88aa2a636dbf669eac
David Tejerina <davidtejerina2003@gmail.com>
2024-04-02 11:03:32 +0200

    primeros cambios, bootstrap y más

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    urlAuth: 'http://localhost:8080/divehub/auth',
+    urlUser: 'http://localhost:8080/divehub/users',
+    urlItem: 'http://localhost:8080/divehub/items',
+    urlActivity: 'http://localhost:8080/divehub/activities',
+    urlCart: 'http://localhost:8080/divehub/carts',
+    urlOrder: 'http://localhost:8080/divehub/orders',
+    urlWish: 'http://localhost:8080/diveHub/wishes'
+  }
