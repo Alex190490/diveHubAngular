0315607d7df4a1120da22436aea67bea6a2e2ddc
David Tejerina <davidtejerina2003@gmail.com>
2024-04-02 11:25:07 +0200

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },  
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'not-found',
+        component: NotFoundComponent
+    },
+    {
+        path: '**',                            // ejemplo: http://localhost:4200/nijcwncpwcmwo 
+        redirectTo: 'not-found'
+    }
+];
+
+
+//EL ORDEN ES MUY IMPORTANTE
