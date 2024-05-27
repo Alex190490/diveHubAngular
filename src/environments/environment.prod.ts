4c346d535fdac2aa1be91019c1460f663a3bf2a5
Alex <alejandroplozano@gmail.com>
2024-05-27 13:19:00 +0200

    despliegue cloudbuild v11 cambios apuntando a bbdd desplegada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-    urlAuth: 'http://localhost:8080/divehub/auth',
-    urlUser: 'http://localhost:8080/divehub/users',
-    urlProducts: 'http://localhost:8080/divehub/products',
-    urlItem: 'http://localhost:8080/divehub/items',
-    urlActivity: 'http://localhost:8080/divehub/activities',
-    urlCart: 'http://localhost:8080/divehub/carts',
-    urlOrder: 'http://localhost:8080/divehub/orders',
-    urlWish: 'http://localhost:8080/divehub/wishes',
-    urlMessage: 'http://localhost:8080/divehub/messages',
-    urlDetail: 'http://localhost:8080/divehub/details',
-    urlAssessments: 'http://localhost:8080/divehub/assessments'
+    urlAuth: 'http://34.22.233.189:8080/divehub/auth',
+    urlUser: 'http://34.22.233.189:8080/divehub/users',
+    urlProducts: 'http://34.22.233.189:8080/divehub/products',
+    urlItem: 'http://34.22.233.189:8080/divehub/items',
+    urlActivity: 'http://34.22.233.189:8080/divehub/activities',
+    urlCart: 'http://34.22.233.189:8080/divehub/carts',
+    urlOrder: 'http://34.22.233.189:8080/divehub/orders',
+    urlWish: 'http://34.22.233.189:8080/divehub/wishes',
+    urlMessage: 'http://34.22.233.189:8080/divehub/messages',
+    urlDetail: 'http://34.22.233.189:8080/divehub/details',
+    urlAssessments: 'http://34.22.233.189:8080/divehub/assessments'
   }
