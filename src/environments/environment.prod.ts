bf03df0534e1b36a4c37781c4ff0c53fff31a345
Alex <alejandroplozano@gmail.com>
2024-05-27 13:49:58 +0200

    despliegue cloudbuild cambios apuntando a bbdd desplegada v3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
-    urlAuth: 'http://angular-app-v1-ew.a.run.app/divehub/auth',
-    urlUser: 'http://angular-app-v1-ew.a.run.app//divehub/users',
-    urlProducts: 'http://angular-app-v1-ew.a.run.app//divehub/products',
-    urlItem: 'http://angular-app-v1-ew.a.run.app//divehub/items',
-    urlActivity: 'http://angular-app-v1-ew.a.run.app//divehub/activities',
-    urlCart: 'http://angular-app-v1-ew.a.run.app//divehub/carts',
-    urlOrder: 'http://angular-app-v1-ew.a.run.app//divehub/orders',
-    urlWish: 'http://angular-app-v1-ew.a.run.app//divehub/wishes',
-    urlMessage: 'http://angular-app-v1-ew.a.run.app//divehub/messages',
-    urlDetail: 'http://angular-app-v1-ew.a.run.app//divehub/details',
-    urlAssessments: 'http://angular-app-v1-ew.a.run.app//divehub/assessments'
+    urlAuth: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/auth',
+    urlUser: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/users',
+    urlProducts: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/products',
+    urlItem: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/items',
+    urlActivity: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/activities',
+    urlCart: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/carts',
+    urlOrder: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/orders',
+    urlWish: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/wishes',
+    urlMessage: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/messages',
+    urlDetail: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/details',
+    urlAssessments: 'http://angular-app-5fltwyyg5q-ew.a.run.app/divehub/assessments'
   }
